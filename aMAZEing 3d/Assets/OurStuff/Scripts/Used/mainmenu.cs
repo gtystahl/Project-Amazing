18d8027b3e620b7148dbaa10cc0685b5d38fa203
gtystahl <43938328+gtystahl@users.noreply.github.com>
2020-04-02 23:38:42 -0500

    This removed all oculus hopefully for the last time
    I made a new project and just took everything we had and moved it over. From now we need to create camera movement then we should be good to go on everything else.

---
+++ aMAZEing 3d/Assets/OurStuff/Scripts/Used/mainmenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public GameObject play;
+    public string selection;
+
+    public void Start()
+    {
+        Debug.Log("hello World");
+        selection = "Play_Button";
+        play.GetComponent<Renderer>().material  = play.GetComponent<materialSwitch>().dif;
+    }
+
+    /*public void FixedUpdate()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.Log("This works");
+            if (selection == "Play_Button")
+            {
+                SceneManager.LoadScene("Game");
+            }
+            else if (selection == "Play_Again")
+            {
+                SceneManager.LoadScene("Game");
+            }
+            else
+            {
+                //quit somehow
+            }
+
+
+            
+        }
+    }
+    */
+}
+
+
