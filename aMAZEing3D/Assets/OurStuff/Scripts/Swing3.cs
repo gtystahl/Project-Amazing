336242a44928f489ddd70490284ddc9ad50b991b
gtystahl <43938328+gtystahl@users.noreply.github.com>
2020-04-28 00:08:10 -0500

    Changed the rotation of the weapon

--- aMAZEing3D/Assets/OurStuff/Scripts/Swing3.cs
+++ aMAZEing3D/Assets/OurStuff/Scripts/Swing3.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class Swing3 : MonoBehaviour
 {
-      public float speed;
+    public float speed;
+    public Vector3 v;
     // Start is called before the first frame update
     void Start()
     {
-        
+        v = new Vector3(0, 90, 0);
     }
     void Update(){
-         if (Input.GetKey(KeyCode.Space)){
-           //transform.Rotate(Vector3.right * speed * Time.deltaTime);
-            if (this.transform.localRotation.eulerAngles.x <= 350){
+       
+        //Debug.Log(this.transform.localEulerAngles);
+         if (Input.GetKeyDown(KeyCode.Space)){
                 //Debug.Log(this.transform.localRotation.eulerAngles.x);
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
-            }
-          }
-          else{
-            if(this.transform.localRotation.eulerAngles.x >=275){
-              transform.Rotate(-Vector3.up * speed * Time.deltaTime);
-            }
+                //Vector3.up and speed
+            transform.Rotate(v);
+         }
+         else if (Input.GetKeyUp(KeyCode.Space)) {
+            //Vector3.up and speed
+            transform.Rotate(-v);
 
         }
+        
+
+        /*
+        Debug.Log(this.transform.localEulerAngles);
+        transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+        */
     }
 }
