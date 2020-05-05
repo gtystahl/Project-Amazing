b2272e95a29109a6b1056525174523e6a9a3b8d5
Jason-Konz <jasonkonz5@gmail.com>
2020-05-05 00:48:32 -0500

    Getting rid of bad files

---
+++ aMAZEing3D/Assets/OurStuff/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float elapsedRunningTime = 0f;
+    private float runningStartTime = 0f;
+    private bool running = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!running)
+        {
+            runningStartTime = Time.time;
+            running = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+            elaspedRunningTime = Time.time - runningStartTime;
+        } 
+    }
+
+    public float GetOverallTime()
+    {
+        return elaspedRunningTime;
+    }
+}
