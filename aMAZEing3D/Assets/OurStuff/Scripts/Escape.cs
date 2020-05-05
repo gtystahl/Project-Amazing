0f8f6da1074cc257c0a3f654f8e440b75a4de3f1
gtystahl <43938328+gtystahl@users.noreply.github.com>
2020-05-05 13:37:58 -0500

    This adds AI work, a debug camera, and the setup for the pause menu

---
+++ aMAZEing3D/Assets/OurStuff/Scripts/Escape.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    public GameObject ac;
+    public List<GameObject> al;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            al = ac.GetComponent<AI_Creater>().ailist;
+            for (int i = 0; i < al.Count; i++)
+            {
+                al[i].GetComponent<AI_Controller>().enabled = false;
+            }
+            //This is where we need to switch the camera
+        }
+
+        /*
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            al = ac.GetComponent<AI_Creater>().ailist;
+            for (int i = 0; i < al.Count; i++)
+            {
+                al[i].GetComponent<AI_Controller>().enabled = true;
+            }
+        }
+        follow this for getting the AI to start again
+        */
+    }
+}
