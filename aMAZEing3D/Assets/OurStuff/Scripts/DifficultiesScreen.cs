1758d6f11f17f3f5f3e99859ee8fec0273f6bb8b
Jason-Konz <jasonkonz5@gmail.com>
2020-05-05 14:45:23 -0500

    This has the difficulties scene made but not fully functional

---
+++ aMAZEing3D/Assets/OurStuff/Scripts/DifficultiesScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DifficultiesScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void EasyMode ()
+{
+    SceneManager.LoadScene("Game");
+}
+
+public void MediumMode ()
+{
+    SceneManager.LoadScene("Game");
+}
+
+public void HardMode ()
+{
+    SceneManager.LoadScene("Game");
+}
+
+}
