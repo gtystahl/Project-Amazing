32921db89d90ce850f3f0033f8b61f43d08726bb
Jason-Konz <jasonkonz5@gmail.com>
2020-05-07 16:04:04 -0500

    This has the timer working on the main game

--- aMAZEing3D/Assets/OurStuff/Scripts/timer.cs
+++ aMAZEing3D/Assets/OurStuff/Scripts/timer.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
+    public Text timertext;
     public float t;
     public int T;
     // Start is called before the first frame update
@@ -19,5 +21,10 @@ public class timer : MonoBehaviour
         t += Time.deltaTime;
         //The T below is the one that needs to be displayed
         T = Mathf.RoundToInt(t);
+
+        string minutes = ((int)t /60).ToString();
+        string seconds =(t%60).ToString("f2");
+
+        timertext.text = minutes + ":" + seconds;
     }
 }
